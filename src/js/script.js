f1188be9ec41756291ea2a47b7065488c21e0a4d
FarhodWebDev <yusupov0902farod@gmail.com>
2023-05-30 15:53:35 +0500

    nav-bar fully completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,20 @@
 
 // BTNS
 const btnOpener = document.querySelector("#nav-opener");
+const btnClose = document.querySelector("#nav-closer");
 
 // ELEMENTS
+const body = document.querySelector("#body");
 const navbar = document.querySelector("#nav-bar");
 
 btnOpener.addEventListener("click", () => {
  navbar.style.display = "block";
+ body.classList.add("no-scroll");
+ btnClose.style.display = "block";
+});
+
+btnClose.addEventListener("click", () => {
+ navbar.style.display = "none";
+ body.classList.remove("no-scroll");
+ btnClose.style.display = "none";
 });
