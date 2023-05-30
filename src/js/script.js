939986a14b74f5cb5c531c38a15c581ae7131c30
FarhodWebDev <yusupov0902farod@gmail.com>
2023-05-30 08:56:29 +0500

    update navbar

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+//
+
+// BTNS
+const btnOpener = document.querySelector("#nav-opener");
+
+// ELEMENTS
+const navbar = document.querySelector("#nav-bar");
+
+btnOpener.addEventListener("click", () => {
+ navbar.style.display = "block";
+});
